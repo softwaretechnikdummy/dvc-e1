18c05763c1d0eb0acd0937b3a243394f086abd2a
s89875 <s89875@bht-berlin.de>
2026-05-03 19:21:47 +0200

    implemented mulberry32 seeded random

--- project/project.js
+++ project/project.js
@@ -1,3 +1,15 @@
-var random_number = Math.random() * 10; // important comment
+// mulberry32 taken from https://github.com/cprosche/mulberry32
+function mulberry32(a) {
+    return function() {
+      var t = a += 0x6D2B79F5;
+      t = Math.imul(t ^ t >>> 15, t | 1);
+      t ^= t + Math.imul(t ^ t >>> 7, t | 61);
+      return ((t ^ t >>> 14) >>> 0) / 4294967296;
+    }
+}
 
-console.log(`randomly chosen number: ${random_number}`);
+const random_mulberry = mulberry32((new Date()).getTime());
+
+var seeded_random = random_mulberry();
+
+console.log(`randomly chosen number: ${seeded_random}`);
